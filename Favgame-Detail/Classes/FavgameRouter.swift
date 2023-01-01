68133548be1c879bfea27775ddb1b70bc6ddcbef
deri.indrawan <deri.indrawan@dwidasa.com>
2023-01-01 08:26:19 +0700

    init commit

---
+++ Favgame-Detail/Classes/FavgameRouter.swift
@@ -0,0 +1,26 @@
+//
+//  DetailRouter.swift
+//  Favgame
+//
+//  Created by deri indrawan on 31/12/22.
+//
+
+import Foundation
+import Favgame-Core
+
+public class DetailRouter {
+  let container: Container = {
+    let container = Injection().container
+    
+    container.register(DetailViewController.self) { resolver in
+      let controller = DetailViewController()
+      controller.getGameDetailUseCase = resolver.resolve(GetGameDetailUseCase.self)
+      controller.insertFavoriteGameUseCase = resolver.resolve(InsertFavoriteGameUseCase.self)
+      controller.checkIsFavoriteUseCase = resolver.resolve(CheckIsFavoriteUseCase.self)
+      controller.deleteFavoriteGameUseCase = resolver.resolve(DeleteFavoriteGameUseCase.self)
+      return controller
+    }
+    return container
+  }()
+}
+
